35604ca86b9914eaba2aaf08762d4b21b48a4508
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2022-05-09 23:55:45 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 8.1.0; CPH1803) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Mobile Safari/537.36";
+
+$cookie = "remember_code=244946a1a9de5a2015b1eaa36ad5bb6fcb503aa9.d94643593d3dd2c6f07520423468a04999b350b5c5fce3b0a2213ab50669e9cebcda4ad18aa885cb61876d30e35be7ffd50c85a4d6e2c4d6656109b03eaabd9c; ci_session=25d78771c3cd1ec9f723716b3e1cfcba332ce8ff";
